45ea3940352d523d90997199b924fb7bd96fca86
darknet33 <74678426+darknet33@users.noreply.github.com>
2023-09-13 13:35:13 -0400

    tiene todas las funciones

--- python/ejemplos.py
+++ python/ejemplos.py
@@ -1,6 +1,30 @@
+numero1=5
+numero2=6
+
 def suma(num1,num2):
     return num1 + num2
 
-resultado=sum(1,6)
+resultado=suma(numero1,numero2)
+
+print (resultado)
+
+def resta(num1,num2):
+    return num1 - num2
+
+resultado=resta(numero1,numero2)
+
+print (resultado)
+
+def multi(num1,num2):
+    return num1 * num2
+
+resultado=multi(numero1,numero2)
+
+print (resultado)
+
+def div(num1,num2):
+    return num1 / num2
+
+resultado=div(numero1,numero2)
 
 print (resultado)
